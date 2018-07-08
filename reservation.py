1eab08f89ab7fa9a0a9ee6ff86040df7e07dd0ee
haesung0125 <haesung0125@naver.com>
2018-07-09 02:24:15 +0900

    added lines to read flag option. added version check.

--- reservation.py
+++ reservation.py
@@ -1,5 +1,9 @@
+import sys
 import datetime
 import requests
+import optparse
+
+assert sys.version_info[0]>=3
 
 '''
 Information needed to reserve a seminar room
@@ -15,33 +19,35 @@ Information needed to reserve a seminar room
 '''
 
 def main():
-    date="2018-07-14"
+
+    parser=optparse.OptionParser()
+    parser.add_option("-d","--date",dest="date",help="Date to reserve. ex)2018-07-09")
+    parser.add_option("-n","--name",dest="name",help="name of the seminar")
+    parser.add_option('-r',"--room",dest="room_number",help="Seminar Room Number. ex)23-312,56-321")
+    parser.add_option('-s',"--start",dest="start_time",help="Seminar starting time in 4 digit number. ex)1000(10 a.m.), 1400(2 p.m.)")
+    parser.add_option('-e',"--end",dest="end_time",help="Seminar ending time in 4 digit number. ex)1000(10 a.m.), 1400(2 p.m.)")
+    parser.add_option('-p',"--prof",dest="professor",help="Name of the Professor")
+    parser.add_option('-c',"--contact",dest="contact",help="Contact Information")
+    parser.add_option('--pw',"--password",dest="password",default="alpine",help="Password needed for cancellation")
+
+    options,args=parser.parse_args()
+
+    date=options['date']
+    name=options['name']
+    room_number=options['room_number']
+    start_time=options['start_time']
+    end_time=options['end_time']
+    prof_name=options['professor']
+    contact=options['contact']
+    password=options['password']
+
     date2=datetime.datetime.strptime(date,"%Y-%m-%d")
     time_delta=datetime.timedelta(days=date2.weekday()+2)
     next_date=(date2-time_delta).strftime('%Y-%m-%d')
 
-    bg_color={'56-219':'a2b461','56-321':'bf9475','56-521':'bf758a','23-317':'9975bf','129-205':'64b0c4'}
-    room_number="23-317"
-    start_time="1000"
-
+    bg_color={'56-219':'a2b461','56-321':'bf9475','56-521':'bf758a','23-317':'9975bf','129-205':'64b0c4','56-322':'8fd175','56-522':'67947a'}
     url="http://phya.snu.ac.kr/php/time_table/inputform_seminar_d.php?date="+date+"&bg_color="+bg_color[room_number]+"&room="+room_number+"&p_first="+start_time+"&next_date="+next_date+"/post"
-
-    name="asdfadsfdsf" #name of the meeting
-    prof_name="" #name of the professor
-    duration="120" #in minutes.
-
-    start_time2=int(start_time)
-    duration2=int(duration)
-    end2=start_time2+100*(duration/60)
-    if(duration==30):
-        end2+=30
-    else:
-        print("Reservation can be done only in 30-minute unit")
-        return 0
     end=str(end2)
-    contact="010-1234-5678" #your contact information
-    password="alpine" #password needed for cancellation
-
     payloads={'subject':name,'host':prof_name,'email':contact,'password':password,"p_end":end}
     r=requests.post(url,data=payloads)
 
