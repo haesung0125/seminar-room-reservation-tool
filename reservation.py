aaf756901dc580910f23cfa0d2999d8aabfd8616
Lee Hae Seong <haesung0125@naver.com>
2018-07-17 12:47:34 +0900

    make reservation job as a function

--- reservation.py
+++ reservation.py
@@ -16,6 +16,26 @@ Information needed to reserve a seminar room
 7. Password
 '''
 
+def reserve(date,name,room_number,start_time,end_time,professor,contact,password):
+    room_list=['56-219','56-321','56-521','23-317','129-205','56-322','56-522']
+    if(room_number not in room_list):
+        print("There is no room numbered "+room_number)
+        return 0
+
+    if(int(end_time)<int(start_time)):
+        print("you can't end something before you start.")
+        return 0
+
+    date2=datetime.datetime.strptime(date,"%Y-%m-%d")
+    time_delta=datetime.timedelta(days=date2.weekday()+2)
+    next_date=(date2-time_delta).strftime('%Y-%m-%d')
+
+    bg_color={'56-219':'a2b461','56-321':'bf9475','56-521':'bf758a','23-317':'9975bf','129-205':'64b0c4','56-322':'8fd175','56-522':'67947a'}
+    url="http://phya.snu.ac.kr/php/time_table/input_seminar_d.php?date="+date+"&bg_color="+bg_color[room_number]+"&room="+room_number+"&p_first="+start_time+"&next_date="+next_date+"/post"
+    payloads={'subject':name,'host':prof_name,'email':contact,'password':password,"p_end":end_time}
+    r=requests.post(url,data=payloads)
+    return r.txt
+
 def main():
 
     if(sys.version_info[0]<3):
@@ -35,33 +55,7 @@ def main():
 
     options,args=parser.parse_args()
 
-    date=options.date
-    name=options.name
-    room_number=options.room_number
-    start_time=options.start_time
-    end_time=options.end_time
-    prof_name=options.professor
-    contact=options.contact
-    password=options.password
-
-    room_list=['56-219','56-321','56-521','23-317','129-205','56-322','56-522']
-    if(room_number not in room_list):
-        print("There is no room numbered "+room_number)
-        return 0
-
-    if(int(end_time)<int(start_time)):
-        print("you can't end something before you start.")
-        return 0
-
-    date2=datetime.datetime.strptime(date,"%Y-%m-%d")
-    time_delta=datetime.timedelta(days=date2.weekday()+2)
-    next_date=(date2-time_delta).strftime('%Y-%m-%d')
-
-    bg_color={'56-219':'a2b461','56-321':'bf9475','56-521':'bf758a','23-317':'9975bf','129-205':'64b0c4','56-322':'8fd175','56-522':'67947a'}
-    url="http://phya.snu.ac.kr/php/time_table/input_seminar_d.php?date="+date+"&bg_color="+bg_color[room_number]+"&room="+room_number+"&p_first="+start_time+"&next_date="+next_date+"/post"
-    payloads={'subject':name,'host':prof_name,'email':contact,'password':password,"p_end":end_time}
-    r=requests.post(url,data=payloads)
-    print(r.text)
+    reserve(options.date,options.name,options.room_number,options.start_time,options.end_time,options.professor,options.contact,options.password)
 
 if(__name__=="__main__"):
     main()
