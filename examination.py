741572b9a17df5ae95675c77ecdbc92ede06c990
Lee Hae Seong <haesung0125@naver.com>
2018-07-18 10:56:13 +0900

    file modification
    renamed cancel_reservation.py to cancellation.py.
    added examination.py to check reservation status largely.

---
+++ examination.py
@@ -0,0 +1,18 @@
+import datetime
+from bs4 import BeautifulSoup
+
+bg_color={'56-219':'a2b461','56-321':'bf9475','56-521':'bf758a','23-317':'9975bf','129-205':'64b0c4','56-322':'8fd175','56-522':'67947a'}
+
+def get_a_week(date):
+
+    pass
+
+
+
+
+
+def main():
+    pass
+
+if(__name__=="__main__"):
+    main()
